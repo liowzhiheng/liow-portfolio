b3c530247d651c23270d20e0b88dd5dda6e7ad70
Liow Zhi Heng <liowzhiheng@gmail.com>
2026-08-07 15:09:07 +0800

    First Draft

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { FooterComponent } from '../footer/footer.component';
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     RouterOutlet,
-    NavbarComponent,
-    FooterComponent
+    NavbarComponent
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
